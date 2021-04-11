5f939aa83c1bbe542c021921ddf4be4637382562
Andreea Trailescu <andreeatrailescu@gmail.com>
2021-04-11 18:10:24 +0300

    change some implementation detalis

--- src/main/java/org/openjfx/exceptions/PasswordIncorrectException.java
+++ src/main/java/org/openjfx/exceptions/PasswordIncorrectException.java
@@ -5,7 +5,7 @@ public class PasswordIncorrectException extends Exception {
     private String password;
 
     public PasswordIncorrectException (String password) {
-        super(String.format("Incorrect password!"));
+        super(String.format("Username already exists or the password entered is incorrect!"));
         this.password = password;
     }
 
