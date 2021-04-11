8f8f674a995d4b2e6c58607958b84da60d83b9ff
Andreea Trailescu <andreeatrailescu@gmail.com>
2021-04-11 17:34:51 +0300

    YDV-22

--- src/main/java/org/openjfx/App.java
+++ src/main/java/org/openjfx/App.java
@@ -12,7 +12,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        Parent root=FXMLLoader.load(getClass().getClassLoader().getResource("hello.fxml"));
+        Parent root=FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
