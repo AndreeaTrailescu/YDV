a56cf54f24c6388ac24f5443c97ebae3c0dbc011
Tuculia Sonia <tuculia_sonia@yahoo.com>
2021-04-22 13:48:17 +0300

    YDV-28 Retrieved the names of the offers from the database

--- src/main/java/org/openjfx/controllers/OffersPageController.java
+++ src/main/java/org/openjfx/controllers/OffersPageController.java
@@ -2,19 +2,27 @@ package org.openjfx.controllers;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
+import org.dizitart.no2.SortOrder;
+import org.dizitart.no2.objects.Cursor;
+import org.dizitart.no2.objects.ObjectRepository;
+import org.openjfx.model.Offer;
+import org.openjfx.services.OfferService;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class OffersPageController {
+import static org.dizitart.no2.FindOptions.sort;
+import static org.dizitart.no2.objects.filters.ObjectFilters.eq;
+
+public class OffersPageController implements Initializable {
     private static String selectedAgency;
 
-    @FXML
-    private Label nameOfAgencyLabel;
     @FXML
     private Button agencyListButton;
     @FXML
@@ -24,6 +32,17 @@ public class OffersPageController {
 
     public static void setSelectedAgency(String selectedAgency) {
         OffersPageController.selectedAgency = selectedAgency;
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        ObjectRepository<Offer> OFFER_REPOSITORY = OfferService.getOfferRepository();
+        Cursor<Offer> offers = OFFER_REPOSITORY.find(eq("nameOfAgency",selectedAgency),sort("nameOfOffer", SortOrder.Ascending));
+        System.out.println(selectedAgency);
+        offers.toList();
+        for(Offer offer : offers){
+            System.out.println(offer.getNameOfOffer());
+        }
     }
 
     @FXML
