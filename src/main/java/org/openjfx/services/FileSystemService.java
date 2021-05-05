9c6a36eaa887a2142eb0a7eeca43fc72e9f7029b
Tuculia Sonia <tuculia_sonia@yahoo.com>
2021-05-05 11:46:21 +0300

    YDV-67 Created tests with Junit 5 for the Add Offer functionality

--- src/main/java/org/openjfx/services/FileSystemService.java
+++ src/main/java/org/openjfx/services/FileSystemService.java
@@ -6,17 +6,25 @@ import java.nio.file.Paths;
 
 public class FileSystemService {
     public static String APPLICATION_FOLDER = ".registration-database";
-    private static final String OFFERS_FOLDER =".offers-database";
+    public static String OFFERS_FOLDER =".offers-database";
     private static final String BOOKINGS_FOLDER =".bookings-database";
     private static final String USER_FOLDER = System.getProperty("user.home");
-    public static final Path OFFERS_HOME_PATH = Paths.get(USER_FOLDER, OFFERS_FOLDER);
     public static final Path BOOKINGS_HOME_PATH = Paths.get(USER_FOLDER, BOOKINGS_FOLDER);
 
     public static Path getPathToFile(String... path) {
         return getApplicationHomeFolder().resolve(Paths.get(".", path));
     }
+
+    public static Path getPathToOffer(String... path) {
+        return getOffersHomeFolder().resolve(Paths.get(".", path));
+    }
+
     public static Path getApplicationHomeFolder() {
         return Paths.get(USER_FOLDER, APPLICATION_FOLDER);
+    }
+
+    public static Path getOffersHomeFolder(){
+        return Paths.get(USER_FOLDER,OFFERS_FOLDER);
     }
 
     static void initDirectory() {
@@ -24,18 +32,17 @@ public class FileSystemService {
         if (!Files.exists(applicationHomePath))
             applicationHomePath.toFile().mkdirs();
 
-        /*Path offersHomePath = FileSystemService.OFFERS_HOME_PATH;
-        if (!Files.exists(offersHomePath))
-            offersHomePath.toFile().mkdirs();
-
-        Path bookingsHomePath = FileSystemService.BOOKINGS_HOME_PATH;
+        /*Path bookingsHomePath = FileSystemService.BOOKINGS_HOME_PATH;
         if (!Files.exists(bookingsHomePath))
             bookingsHomePath.toFile().mkdirs();*/
     }
 
-    public static Path getPathToOffer(String... path) {
-        return OFFERS_HOME_PATH.resolve(Paths.get(".", path));
+    static void initOffersDirectory(){
+        Path offersHomePath = FileSystemService.getOffersHomeFolder();
+        if (!Files.exists(offersHomePath))
+            offersHomePath.toFile().mkdirs();
     }
+
     public static Path getPathToBooking(String... path) {
         return BOOKINGS_HOME_PATH.resolve(Paths.get(".", path));
     }
